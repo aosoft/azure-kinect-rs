5085221cf898d3b59b1a73e6d50bf012d735be77
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-05-10 14:41:24 +0900

    Implement Calibration

--- src/calibration.rs
+++ src/calibration.rs
@@ -34,4 +34,71 @@ impl Calibration<'_> {
             .to_result(target_point3d)
         }
     }
+
+    pub fn convert_2d_to_3d(
+        &self,
+        source_point2d: &k4a_float2_t,
+        source_depth: f32,
+        source_camera: k4a_calibration_type_t,
+        target_camera: k4a_calibration_type_t,
+    ) -> Result<(k4a_float3_t, bool), Error> {
+        unsafe {
+            let mut target_point3d = k4a_float3_t::default();
+            let mut valid: i32 = 0;
+            Error::from((self.factory.k4a_calibration_2d_to_3d)(
+                &self.calibration,
+                source_point2d,
+                source_depth,
+                source_camera,
+                target_camera,
+                &mut target_point3d,
+                &mut valid,
+            ))
+            .to_result((target_point3d, valid != 0))
+        }
+    }
+
+    pub fn convert_3d_to_2d(
+        &self,
+        source_point3d: &k4a_float3_t,
+        source_camera: k4a_calibration_type_t,
+        target_camera: k4a_calibration_type_t,
+    ) -> Result<(k4a_float2_t, bool), Error> {
+        unsafe {
+            let mut target_point2d = k4a_float2_t::default();
+            let mut valid: i32 = 0;
+            Error::from((self.factory.k4a_calibration_3d_to_2d)(
+                &self.calibration,
+                source_point3d,
+                source_camera,
+                target_camera,
+                &mut target_point2d,
+                &mut valid,
+            ))
+            .to_result((target_point2d, valid != 0))
+        }
+    }
+
+    pub fn convert_2d_to_2d(
+        &self,
+        source_point2d: &k4a_float2_t,
+        source_depth: f32,
+        source_camera: k4a_calibration_type_t,
+        target_camera: k4a_calibration_type_t,
+    ) -> Result<(k4a_float2_t, bool), Error> {
+        unsafe {
+            let mut target_point2d = k4a_float2_t::default();
+            let mut valid: i32 = 0;
+            Error::from((self.factory.k4a_calibration_2d_to_2d)(
+                &self.calibration,
+                source_point2d,
+                source_depth,
+                source_camera,
+                target_camera,
+                &mut target_point2d,
+                &mut valid,
+            ))
+            .to_result((target_point2d, valid != 0))
+        }
+    }
 }
