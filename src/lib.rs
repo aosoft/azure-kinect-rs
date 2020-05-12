486de29ec2f92b83f60dddd408c360acbf1a3499
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-05-12 13:55:17 +0900

    update test

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-pub mod error;
-pub mod default;
-pub mod display;
-pub mod k4a_functions;
-pub mod utility;
-pub mod factory;
 pub mod calibration;
-pub mod capture;
-pub mod device;
 pub mod camera;
-pub mod imu;
+pub mod capture;
+pub mod default;
+pub mod device;
+pub mod display;
+pub mod error;
+pub mod factory;
 pub mod image;
+pub mod imu;
+pub mod k4a_functions;
 pub mod transformation;
+pub mod utility;
 
 pub mod bindings {
     include!("bindings.rs");
@@ -22,8 +22,8 @@ pub mod bindings {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use super::bindings::*;
+    use super::*;
 
     extern "C" fn debug_message_handler(
         _: *mut ::std::os::raw::c_void,
@@ -31,7 +31,7 @@ mod tests {
         file: *const ::std::os::raw::c_char,
         line: ::std::os::raw::c_int,
         message: *const ::std::os::raw::c_char,
-    ){
+    ) {
         unsafe {
             let file2 = std::ffi::CStr::from_ptr(file).to_str().unwrap_or("");
             let message2 = std::ffi::CStr::from_ptr(message).to_str().unwrap_or("");
@@ -51,7 +51,8 @@ mod tests {
         factory.set_debug_message_handler(
             Some(debug_message_handler),
             std::ptr::null_mut(),
-            k4a_log_level_t::K4A_LOG_LEVEL_ERROR)?;
+            k4a_log_level_t::K4A_LOG_LEVEL_ERROR,
+        )?;
 
         let c = factory.device_get_installed_count();
         println!("device count = {}", c);
@@ -67,9 +68,26 @@ mod tests {
 
         let camera_config = k4a_device_configuration_t::default();
         let camera = device.start_cameras(&camera_config)?;
-        let imu = camera.start_imu()?;
-        let imu_sample = imu.get_imu_sample_wait_infinite()?;
-        println!("imu = {}", imu_sample);
+
+        {
+            let imu = camera.start_imu()?;
+            let imu_sample = imu.get_imu_sample_wait_infinite()?;
+            println!("imu = {}", imu_sample);
+        }
+
+        for i in 0..20 {
+            if let Ok(capture) = camera.get_capture(100) {
+                let image = capture.get_color_image();
+                println!(
+                    "[{}] format = {:?}, width = {}, height = {}, temparature = {}",
+                    i,
+                    image.get_format(),
+                    image.get_width_pixels(),
+                    image.get_height_pixels(),
+                    capture.get_temperature_c()
+                );
+            }
+        }
 
         Ok(())
     }
