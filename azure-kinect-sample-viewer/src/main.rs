c72091e2be933000ac9cb53e341d55dfb819764d
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-05-19 01:15:46 +0900

    Implement depth-view test

--- azure-kinect-sample-viewer/src/main.rs
+++ azure-kinect-sample-viewer/src/main.rs
@@ -12,15 +12,15 @@ fn main() {
 fn main2() -> Result<(), Box<dyn std::error::Error>> {
     let factory = Factory::new()?;
     let device = factory.device_open(0)?;
-    let camera_config = k4a_device_configuration_t::default();
+    let camera_config = k4a_device_configuration_t {
+        depth_mode: k4a_depth_mode_t::K4A_DEPTH_MODE_NFOV_2X2BINNED,
+        ..k4a_device_configuration_t::default()};
     let camera = device.start_cameras(&camera_config)?;
 
     #[cfg(feature = "depth-view")]
-    let calibration = device.get_calibration(camera_config.depth_mode, camera_config.color_resolution)?;
-    #[cfg(feature = "depth-view")]
-    let transformation = Transformation::new(&factory, &calibration);
-
-    let color_image_dimension = camera_config.color_resolution.get_dimension();
+    let image_dimension = camera_config.depth_mode.get_dimension();
+    #[cfg(not(feature = "depth-view"))]
+    let image_dimension = camera_config.color_resolution.get_dimension();
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -28,8 +28,8 @@ fn main2() -> Result<(), Box<dyn std::error::Error>> {
     let window = video_subsystem
         .window(
             "azure-kinect-sample-viewer",
-            color_image_dimension.width as u32,
-            color_image_dimension.height as u32,
+            image_dimension.width as u32,
+            image_dimension.height as u32,
         )
         .position_centered()
         .opengl()
@@ -40,8 +40,8 @@ fn main2() -> Result<(), Box<dyn std::error::Error>> {
     let mut texture = texture_creator
         .create_texture_streaming(
             PixelFormatEnum::ARGB8888,
-            color_image_dimension.width as u32,
-            color_image_dimension.height as u32,
+            image_dimension.width as u32,
+            image_dimension.height as u32,
         )
         .map_err(|e| e.to_string())?;
 
@@ -63,17 +63,16 @@ fn main2() -> Result<(), Box<dyn std::error::Error>> {
                 #[cfg(feature = "depth-view")]
                 {
                     let depth_image = capture.get_depth_image();
-                    let cvtd = transformation.depth_image_to_color_camera(&depth_image);
-                    if let Ok(cvtd) = cvtd {
-                        let width = cvtd.get_width_pixels();
-                        unsafe {
-                            for y in 0..cvtd.get_height_pixels() as usize {
-                                let p = cvtd.get_buffer().add(y * cvtd.get_stride_bytes() as usize) as *const u16;
-                                let p2 = buffer.as_mut_ptr().add(y * pitch) as *mut u32;
-                                for x in 0..width as isize {
-                                    let value = *p.offset(x);
-                                    *p2.offset(x) = 0xff000000 | value as u32;
-                                }
+                    let width = depth_image.get_width_pixels();
+                    unsafe {
+                        for y in 0..depth_image.get_height_pixels() as usize {
+                            let p = depth_image.get_buffer().add(y * depth_image.get_stride_bytes() as usize)
+                                as *const u16;
+                            let p2 = buffer.as_mut_ptr().add(y * pitch) as *mut u32;
+                            for x in 0..width as isize {
+                                let value = *p.offset(x);
+                                *p2.offset(x) =
+                                    get_depth_color(value, camera_config.depth_mode.get_range())
                             }
                         }
                     }
@@ -103,4 +102,33 @@ fn main2() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+fn get_depth_color(depth: u16, minmax: Range<u16>) -> u32 {
+    if depth == 0 {
+        return 0xff000000;
+    }
+
+    let clamped_value = std::cmp::min(minmax.max, std::cmp::max(depth, minmax.min));
+
+    const RANGE: f32 = 2.0 / 3.0;
+    let hue =
+        RANGE - (clamped_value - minmax.min) as f32 / (minmax.max - minmax.min) as f32 * RANGE;
+
+    let i = (hue * 6.0) as i32;
+    let f = hue * 6.0 - i as f32;
+
+    let rgb = match i {
+        0 => (1.0f32, f, 0.0f32),
+        1 => (1.0 - f, 1.0f32, 0.0f32),
+        2 => (0.0f32, 1.0f32, f),
+        3 => (0.0f32, 1.0 - f, 1.0f32),
+        4 => (f, 0.0f32, 1.0f32),
+        _ => (1.0f32, 0.0f32, 1.0 - f),
+    };
+
+    0xff000000
+        | (((255.0 * rgb.0) as u32) << 16)
+        | (((255.0 * rgb.1) as u32) << 8)
+        | ((255.0 * rgb.2) as u32)
 }
--- azure-kinect/src/format.rs
+++ azure-kinect/src/format.rs
@@ -1,10 +1,12 @@
 use super::*;
 
+#[derive(Copy, Clone, Debug)]
 pub struct Dimension {
     pub width: i32,
     pub height: i32,
 }
 
+#[derive(Copy, Clone, Debug)]
 pub struct Range<T> {
     pub min: T,
     pub max: T,
