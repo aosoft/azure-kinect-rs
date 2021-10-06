c523576afca3cbd194d88ccee383f6dd296098fb
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-10-07 08:22:51 +0900

    Improve bindgen processing

--- azure-kinect-sys/bindgen.sh
+++ azure-kinect-sys/bindgen.sh
@@ -2,22 +2,22 @@
 
 bindgen \
   --output ./src/bindgen_k4a.rs \
-  --allowlist-function k4a_.*\
+  --allowlist-function \(?i\)k4a_.* \
+  --allowlist-var \(?i\)k4a_.* \
   --dynamic-loading Funcs \
   --dynamic-link-require-all \
   --with-derive-default \
-  --no-derive-debug \
   ./include/k4a/k4a.h -- -I ./include -fms-extensions
 sed -i -n "/impl Funcs/q;p" ./src/bindgen_k4a.rs
 sed -i -e "/libloading/d" ./src/bindgen_k4a.rs
 
 bindgen \
   --output ./src/bindgen_k4arecord.rs \
-  --allowlist-function k4a_\(record\|playback\)_.*\
+  --allowlist-function k4a_\(record\|playback\)_.* \
+  --allowlist-var \(?i\)k4a_track_.* \
   --dynamic-loading Funcs \
   --dynamic-link-require-all \
   --with-derive-default \
-  --no-derive-debug \
   wrapper.h -- -I ./include -fms-extensions
 sed -i -n "/impl Funcs/q;p" ./src/bindgen_k4arecord.rs
 sed -i -e "/libloading/d" ./src/bindgen_k4arecord.rs
