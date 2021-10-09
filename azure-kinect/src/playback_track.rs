44ff6718cc295ba43924d746a760cc430186d50b
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-10-09 18:25:09 +0900

    Define unsafe

--- azure-kinect/src/playback_track.rs
+++ azure-kinect/src/playback_track.rs
@@ -23,55 +23,55 @@ impl PlaybackTrack<'_> {
 
     /// Checks whether a track with the given track name exists in the playback file.
     pub fn check_exists(&self) -> bool {
-        (self.playback.api_record.funcs.k4a_playback_check_track_exists)(
+        unsafe { (self.playback.api_record.funcs.k4a_playback_check_track_exists)(
             self.playback.handle,
             self.name.as_ptr(),
-        )
+        ) }
     }
 
     /// Checks whether a track is one of the built-in tracks: "COLOR", "DEPTH", etc...
     pub fn is_builtin(&self) -> bool {
-        (self.playback.api_record.funcs.k4a_playback_track_is_builtin)(
+        unsafe { (self.playback.api_record.funcs.k4a_playback_track_is_builtin)(
             self.playback.handle,
             self.name.as_ptr(),
-        )
+        ) }
     }
 
     /// Gets the video-specific track information for a particular video track.
     pub fn get_video_settings(&self) -> Result<k4a_record_video_settings_t, Error> {
         let mut settings = k4a_record_video_settings_t::default();
-        Error::from_k4a_result_t((self
+        Error::from_k4a_result_t(unsafe { (self
             .playback
             .api_record.funcs
             .k4a_playback_track_get_video_settings)(
             self.playback.handle,
             self.name.as_ptr(),
             &mut settings,
-        ))
+        ) } )
         .to_result(settings)
     }
 
     /// Gets the codec id string for a particular track.
     pub fn get_codec_id(&self) -> Result<String, Error> {
         get_k4a_string(&|codec_id, codec_id_size| {
-            (self.playback.api_record.funcs.k4a_playback_track_get_codec_id)(
+            unsafe { (self.playback.api_record.funcs.k4a_playback_track_get_codec_id)(
                 self.playback.handle,
                 self.name.as_ptr(),
                 codec_id,
                 codec_id_size,
-            )
+            ) }
         })
     }
 
     /// Gets the codec context for a particular track.
     pub fn get_codec_context(&self) -> Result<Vec<u8>, Error> {
         get_k4a_binary_data(&|codec_context, codec_context_size| {
-            (self.playback.api_record.funcs.k4a_playback_track_get_codec_context)(
+            unsafe { (self.playback.api_record.funcs.k4a_playback_track_get_codec_context)(
                 self.playback.handle,
                 self.name.as_ptr(),
                 codec_context,
                 codec_context_size,
-            )
+            ) }
         })
     }
 }
